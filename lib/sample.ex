195688e75b96651b81eaf18aea6e53fd56e6ac88
Yuki Ito <yuki@gnnk.net>
2013-12-01 17:20:59 +0900

    mix dynamo sample

---
+++ lib/sample.ex
@@ -0,0 +1,11 @@
+defmodule Sample do
+  use Application.Behaviour
+
+  @doc """
+  The application callback used to start this
+  application and its Dynamos.
+  """
+  def start(_type, _args) do
+    Sample.Dynamo.start_link([max_restarts: 5, max_seconds: 5])
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,23 @@
+defmodule Sample.Mixfile do
+  use Mix.Project
+
+  def project do
+    [ app: :sample,
+      version: "0.0.1",
+      build_per_environment: true,
+      dynamos: [Sample.Dynamo],
+      compilers: [:elixir, :dynamo, :app],
+      deps: deps ]
+  end
+
+  # Configuration for the OTP application
+  def application do
+    [ applications: [:cowboy, :dynamo],
+      mod: { Sample, [] } ]
+  end
+
+  defp deps do
+    [ { :cowboy, github: "extend/cowboy" },
+      { :dynamo, "~> 0.1.0-dev", github: "elixir-lang/dynamo" } ]
+  end
+end
