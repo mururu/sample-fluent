fd478d28b14b8c6d1ff7595ab7c21016d69a9060
Yuki Ito <yuki@gnnk.net>
2013-12-01 17:57:46 +0900

    Add fluent

--- mix.exs
+++ mix.exs
@@ -18,6 +18,7 @@ defmodule Sample.Mixfile do
 
   defp deps do
     [ { :cowboy, github: "extend/cowboy" },
-      { :dynamo, "~> 0.1.0-dev", github: "elixir-lang/dynamo" } ]
+      { :dynamo, "~> 0.1.0-dev", github: "elixir-lang/dynamo" },
+      { :fluent, github: "mururu/fluent-logger-elixir"}]
   end
 end
